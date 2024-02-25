0c994d9e9f4399e4a54e1b38ce6b9e3a07248e08
youngboypimpin <rjtekdom@gmail.com>
2024-02-26 00:53:16 +0100

    minor changes installed react router setting up my project files for routing will complete in next commit on app.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // Import your main App component or the component you want to render
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
